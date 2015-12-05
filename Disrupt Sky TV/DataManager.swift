aed5de38dfe446578e0133a471f5933bbb5b4b7a
Oliver Binns <mail@oliverbinns.info>
2015-12-05 19:37:12 +0000

    Work on DataManager

--- Disrupt Sky TV/DataManager.swift
+++ Disrupt Sky TV/DataManager.swift
@@ -7,7 +7,83 @@
 //
 
 import UIKit
+import MapKit
 
 class DataManager: NSObject {
+    class var sharedInstance: DataManager {
+        struct Static {
+            static let instance: DataManager = DataManager()
+        }
+        return Static.instance
+    }
 
+    func apiRequest(location: CLLocationCoordinate2D, callback: (succeeded: Bool, msg: String) -> ()){
+        let defaults = NSUserDefaults.standardUserDefaults();
+        let subscriberId = defaults.stringForKey("subscriberId")!;
+        let deviceId = defaults.stringForKey("deviceId")!;
+        let locationString = String(format: "%d;%d", location.latitude, location.longitude);
+        
+        let params = ["location":locationString, "deviceId":deviceId, "subscriberId":subscriberId] as Dictionary<String, String>
+        
+        post(params, url: "localhost:8080", postCompleted: callback)
+    }
+    
+    private func post(params : Dictionary<String, String>, url : String, postCompleted : (succeeded: Bool, msg: String) -> ()) {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        let session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        do{
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: []);
+        }catch{
+            request.HTTPBody = NSData();
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            print("Response: \(response)")
+            let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("Body: \(strData)")
+            
+            var json: NSDictionary?;
+            do{
+                json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
+            }catch{
+            }
+                
+            //var msg = "No message"
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            /*if(err != nil) {
+                print(err!.localizedDescription)
+                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print("Error could not parse JSON: '\(jsonStr)'")
+                postCompleted(succeeded: false, msg: "Error")
+            }
+            else {*/
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    if let success = parseJSON["success"] as? Bool {
+                        //print("Success: \(success)")
+                        postCompleted(succeeded: success, msg: "Logged in.")
+                    }
+                    return
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print("Error could not parse JSON: \(jsonStr)")
+                    postCompleted(succeeded: false, msg: "Error")
+                }
+            //}
+        })
+        
+        task.resume()
+    }
+    
+    
+    
 }
