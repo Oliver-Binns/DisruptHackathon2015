a84aefb1147a0e1e9bd137415ccfc95b2b157c7b
Andrew-Walker <andreww500@live.com>
2015-12-05 19:33:42 +0000

    Added collection view + navigation bar buttons.

---
+++ Disrupt Sky TV/ResumeCell.swift
@@ -0,0 +1,13 @@
+//
+//  ResumeCell.swift
+//  Disrupt Sky TV
+//
+//  Created by Andrew Walker on 05/12/2015.
+//  Copyright © 2015 Andrew Walker. All rights reserved.
+//
+
+import UIKit
+
+class ResumeCell: UICollectionViewCell {
+    
+}
